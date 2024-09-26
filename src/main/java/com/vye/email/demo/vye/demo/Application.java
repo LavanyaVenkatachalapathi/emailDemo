1ba51ca4a4c33466492f89add1f88c2ef625b86b
venkatachalapathi.o <venkatachalapati.o@hhstechgroup.com>
2024-09-26 12:26:47 +0530

    first commit

---
+++ src/main/java/com/vye/email/demo/vye/demo/Application.java
@@ -0,0 +1,13 @@
+package com.vye.email.demo.vye.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vye/email/demo/vye/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vye.email.demo.vye.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
